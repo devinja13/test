3aa4b6eb4d161c334a9ad4c4993647db1e035ec6
devin <dja6@rice.edu>
2025-08-11 16:41:37 -0500

    a

--- REMS-Scheduling-Optimizer-INFORMS/rems-model-web-app/backend/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from src.api import app
-
-from dotenv import load_dotenv
-load_dotenv()
-
-
-if __name__ == "__main__":
-    uvicorn.run("src.api:app", host="127.0.0.1", port=8000, reload=True) #change this to host 0.0.0.0 when pushed to production
