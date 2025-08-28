b7d1d93217650f0dc0798a8be159bf1edc94a2b5
devin <dja6@rice.edu>
2025-08-28 12:05:28 -0500

    updates

--- rems-model-web-app/backend/main.py
+++ rems-model-web-app/backend/main.py
@@ -7,6 +7,5 @@ load_dotenv()
 
 
 if __name__ == "__main__":
-    uvicorn.run("src.api:app", host="127.0.0.1", port=8000, reload=True) #change this to host 0.0.0.0 when pushed to production
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("src.api:app", host="0.0.0.0", port=port)
+    uvicorn.run("src.api:app", host="0.0.0.0", port=8000, reload=True) #change this to host 0.0.0.0 when pushed to production
+    
