de4feb5ce8d628f2d8ed663918a83a06a4dc9c94
devin <dja6@rice.edu>
2025-08-28 12:17:52 -0500

    updates

--- rems-model-web-app/backend/main.py
+++ rems-model-web-app/backend/main.py
@@ -2,10 +2,19 @@ import os
 import uvicorn
 from src.api import app
 
+
 from dotenv import load_dotenv
 load_dotenv()
 
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://aug-28.vercel.app/"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 if __name__ == "__main__":
     uvicorn.run("src.api:app", host="0.0.0.0", port=8000, reload=True) #change this to host 0.0.0.0 when pushed to production
     
